46739702fa3f58e65959a2aacd6da2a1f6e9c818
Niedzwiedz1990 <domagalski1990@gmail.com>
2021-08-22 11:26:06 +0200

    [change] - project scafolding

---
+++ src/main/java/com/example/cryptokrypto/service/WeatherService.java
@@ -0,0 +1,15 @@
+package com.example.cryptokrypto.service;
+
+import com.example.cryptokrypto.repository.WeatherRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WeatherService {
+
+    private WeatherRepository weatherRepository;
+
+
+    public WeatherService(final WeatherRepository weatherRepository) {
+        this.weatherRepository = weatherRepository;
+    }
+}
